b014d80ae9b67439213929ec24c3386b23630463
rian-gustandi <rian.gustandi17@gmail.com>
2020-05-25 12:38:08 +0700

    Adding webpack-dev-server

--- webpack.common.js
+++ webpack.common.js
@@ -9,26 +9,13 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
-  mode: 'production',
   module: {
     rules: [
       {
         test: /\.css$/,
         use: [
           { loader: 'style-loader' },
-          { loader: 'css-loader' }
-        ]
-      },
-      {
-        test: /\.js$/,
-        exclude: "/node_modules/",
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env']
-            }
-          }
+          { loader: 'css-loader' },
         ]
       }
     ]
@@ -39,5 +26,4 @@ module.exports = {
       filename: 'index.html'
     })
   ]
-
 }
